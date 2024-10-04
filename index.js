66c20142baf8c01a4498cdcdd556d7812480f8b5
asdasd <149241521+ecrww@users.noreply.github.com>
2024-10-04 14:13:30 +0300

    the bot Company

---
+++ index.js
@@ -0,0 +1,12 @@
+let ip = 3.39456234243243254234242394234829048208420948209384329840928409284289402840928402840982409284092842098409284092848204820482025445444545454545454545454545454545445454545454545444545454544 / 6;
+let eee;
+let asdasd;
+
+document.getElementById('asdasdp').onclick = function() {
+  eee = document.getElementById('asdasdq').value;
+  eee = Number(eee);
+
+  asdasd = 2 * ip * eee;
+
+  document.getElementById("aaaaaa").textContent = asdasd + " cm";
+};
