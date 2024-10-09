8f47edd7ee350788f5fd3e7ed447b5214da42db0
asdasd <149241521+ecrww@users.noreply.github.com>
2024-10-09 23:53:32 +0300

    the bot Company

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ document.getElementById('feedback').onclick = function(){
   eeeeee = document.getElementById('feedback1').value;
 
 
-document.getElementById('ha').textContent = `(Thank you  Give us feedback ${eeeeee})`
+document.getElementById('ha').textContent = `(Thank you  Give us feedback (${eeeeee}))`
 
 
 
